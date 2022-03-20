4c7e722bfbf36e8ece8f682abe1533f54ba534ef
Nikolay Makeev <reso1@yandex.ru>
2022-03-20 03:31:03 +0300

    Task 2 Version 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,62 +1,70 @@
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
-import com.opencsv.CSVReader;
-import com.opencsv.bean.ColumnPositionMappingStrategy;
-import com.opencsv.bean.CsvToBean;
-import com.opencsv.bean.CsvToBeanBuilder;
-import org.json.simple.JSONObject;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
-
-import java.io.FileReader;
-import java.io.FileWriter;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
-        String fileName = "data.csv";
-        List<Employee> list = parseCSV(columnMapping, fileName);
-        String json = listToJson(list);
-        System.out.println(json);
-        writeString(json);
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+        File fileName = new File("data.xml");
+        parseXML(fileName);
+
     }
 
-    public static List<Employee> parseCSV(String[] columnMapping, String fileName) {
-        List<Employee> myList = null;
-        try (CSVReader reader = new CSVReader(new FileReader(fileName))) {
-            ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
-            strategy.setType(Employee.class);
-            strategy.setColumnMapping(columnMapping);
-            CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(reader)
-                    .withMappingStrategy(strategy)
-                    .build();
-            myList = csv.parse();
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static List<Employee> parseXML(File fileName) throws ParserConfigurationException, IOException, SAXException {
+        List<Employee> myList = new ArrayList<Employee>();
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(fileName);
+        Node root = doc.getDocumentElement(); // отдал [staff: null]
+        NodeList nodeList = root.getChildNodes();
+        for (int i = 0; i < nodeList.getLength(); i++) { // длина 5 элементов (2 employee и #text обертка какая-то)
+            Node nodeSan = nodeList.item(i);
+            if (Node.ELEMENT_NODE != nodeSan.getNodeType()) continue;
+            System.out.println(nodeSan.getNodeName());
+            NodeList nodeList1 = nodeSan.getChildNodes();
+             long id = 0;
+             String firstName = null;
+             String lastName = null;
+             String country = null;
+             int age = 0;
+
+            for (int j = 0; j < nodeList1.getLength(); j++) {
+                Node nodeSan2 = nodeList1.item(j);
+                if (Node.ELEMENT_NODE == nodeSan2.getNodeType()) {  //отдает узлы без #text
+         //           System.out.println(nodeSan2.getNodeName());
+                    Element element = (Element) nodeSan2;
+                    String nodeValue = element.getTextContent(); // это кошмар( хоть бы намекнули
+                    //The getNodeValue method in the DOM is defined to always return null for element nodes
+                    // (see the table at the top of the JavaDoc page for org.w3c.dom.Node for details).
+                    // If you want the text inside the element then you should use getTextContent() instead.
+                     if (j==0) {
+                         String newId = element.getTextContent();
+                         id = Long.parseLong(newId);
+                     }
+                     if (j==1) firstName = (String) element.getTextContent();
+                     if (j==2) lastName = (String) element.getTextContent();
+                     if (j==3) country = (String) element.getTextContent();
+                     if (j==4) {
+                         String newAge = element.getTextContent();
+                          age = Integer.parseInt(newAge);
+                     }
+                }
+            }
+            Employee employee = new Employee(id, firstName,lastName, country, age);
+            myList.add(employee);
         }
-       return myList;
+          return myList;
     }
-
-    public static String listToJson(List<Employee> list) {
-
-        Type listType = new TypeToken<List<Employee>>() {}.getType();
-        GsonBuilder builder = new GsonBuilder();
-        Gson gson = builder.create();
-        String json = gson.toJson(list, listType);
-        return json;
-    }
-
-    public static void writeString(String json) {
-        try ( FileWriter file = new FileWriter("newfile.json")) {
-            file.write(json);
-            file.flush();
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-    }
-
 }
+
+
+
+
+
